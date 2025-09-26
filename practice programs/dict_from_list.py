7de6c4112f5d68e2e32c8e4bd01705341638cfd9
Ashish Kumar <ashishkumar683768@gmail.com>
2025-09-26 22:50:13 +0530

    deleted folder

--- practice programs/dict_from_list.py
+++
@@ -1,6 +0,0 @@
-# Build dict from list of (name, marks) tuples
-def create_dict(arr):
-    d = {}
-    for name, marks in arr:
-        d[name] = marks
-    return d
--- practice programs/number_square.py
+++
@@ -1,7 +0,0 @@
-# Print numbers 1..x² (squares ≤ limit)
-def squares_upto(x: int):
-    i = 1
-    while i*i <= x:
-        print(i*i, end=" ")
-        i += 1
-    print()
